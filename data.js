615418ec14318254c8f8cbd6928f6722b365afcf
ivan pan <ipan30@stuy.edu>
2022-08-17 15:55:58 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,20 @@
+
+// //let data;
+
+// fetch('https://randomuser.me/api?results=50')
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(myJson){
+//         console.log(myJson.results);
+//         for(let i = 0; i < myJson.results[i].emai; i ++){
+//         //LAST WEEK
+//         let tempEmail = myJson.results[i].email;
+//         let tempP = document.createElement('p');
+//         let bodyRef = document.querySelector('body');
+//         tempP.innerHTML = tempEmail;
+//         bodyRef.appendChild(tempP);
+//         }
+
+//     })
+
---
+++ script.js
@@ -0,0 +1,27 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-73.945631, 40.807114]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'Washington, D.C.'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [-122.414, 37.776]
+        },
+        properties: {
+          title: 'Mapbox',
+          description: 'San Francisco, California'
+        }
+      }
+    ]
+};
